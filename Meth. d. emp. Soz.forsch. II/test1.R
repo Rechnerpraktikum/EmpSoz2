0b8b6b6fa6dd43303cdfbbda72bf28663c944425
maxeyy <maxey@unterderbruecke.de>
2017-06-07 14:53:19 +0200

    update pca

--- Meth. d. emp. Soz.forsch. II/test1.R
+++ Meth. d. emp. Soz.forsch. II/test1.R
@@ -1,10 +1,12 @@
 x <- read.table("dataset_test.csv", header=TRUE, sep=";")
-x1 <- x[,2:48]
-
 
 # Antworten einer Frage umkehren (da negative Frage)
-cols = c("C7", "C14", "F26")
+cols = c("C7", "C14" , "F26")
 x[ ,cols] = 5 - x[ ,cols]
+
+# Relevante Werte PCA
+x1 <- x[,c(14:27,29:41, 43:48)]
+x1
 
 
 
@@ -17,12 +19,18 @@ bart_spher(x1)
 
 VSS.scree(x1)
 
-pca.x1 <- principal(x1, 11, rotate = "none")
+pca.x1 <- principal(x1, nfactors = 6, rotate = "varimax")
 pca.x1
 
 
+print(KMOS(x1), vars = 1:10, sort = TRUE)
 
 
+pca.x1 <- principal(x1, 6)
+pca.x1$criteria <- NULL
+print(pca.x1, sort = TRUE, cut = 0.5, digits =2)
+
+fa.diagram(pca.x1, cut = 0.5, cex = 0.8, rsize = 0.5, main="titel")
 
 b <- cbind(x$F27,x$B3,x$B5,x$E15)
 b
