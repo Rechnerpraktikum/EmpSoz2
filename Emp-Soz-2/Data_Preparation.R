ecd216c6d568d4b7cc25e436cbc0d11775ef6e27
Bernhard <Rechnerpraktikum@outlook.at>
2017-06-14 10:40:35 +0200

    Data Preparation adjusted

--- Emp-Soz-2/Data_Preparation.R
+++ Emp-Soz-2/Data_Preparation.R
@@ -1,9 +1,10 @@
-x <- read.table("dataset_test.csv", header=TRUE, sep=";")
-x1 <- x[,2:48]
+dataset <- read.table("data/dataset.csv", header=TRUE, sep=";")
+attach(dataset)
+
+# Auswahl von Spalte 2 bis 48 (-> nur die Likert Skalen)
+spalte2bis48 <- dataset[,2:48]
 
 
 # Antworten einer Frage umkehren (da negative Frage)
 cols = c("C7", "C14", "F26")
-x[ ,cols] = 5 - x[ ,cols]
-
-#Bartlett Test?
+dataset[ ,cols] = 5 - x[ ,cols]
